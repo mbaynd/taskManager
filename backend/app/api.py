a3b1391ca229e0e2c35953924af7a7a5a157edd0
Mbaye NDIAYE <mbaynd@gmail.com>
2024-05-12 20:34:53 +0000

    update Apps

--- backend/app/api.py
+++ backend/app/api.py
@@ -5,7 +5,8 @@ origins = [
     "http://localhost:3000",
     "localhost:3000",
     "http://192.168.1.9:3000",  
-    "http://192.168.1.10:3000"
+    "http://192.168.1.10:3000",
+    "*"
 ]
 
 app = FastAPI()
